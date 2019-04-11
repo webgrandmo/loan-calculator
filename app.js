060b0b686f18e1903377b93ed5a84a8f7585eccb
Andriy Trofimov <andriy.trofimov@carus.com>
2019-04-11 21:14:51 +0300

    feature/Add-calculation-functionality. Implement calculation and error message

--- app.js
+++ app.js
@@ -2,14 +2,53 @@ document.getElementById('loan-form').addEventListener('submit', calculateResult)
 
 function calculateResult(e) {
 
-    //UI vars
+    //UI varscons
     const amount = document.getElementById('amount');
     const interest = document.getElementById('interest');
     const years = document.getElementById('years');
     const monthlyPayment = document.getElementById('monthly-payment');
     const totalPayment = document.getElementById('total-payment');
-    const totalInterest = document.getElementById('monthly-interest');
+    const totalInterest = document.getElementById('total-interest');
     console.log('Submitted');
 
+    const principal = parseFloat(amount.value);
+    const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+    const calculatedPayments = parseFloat(years.value) * 12;
+
+
+    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+    const monthly = (principal * x * calculatedInterest / (x - 1));
+
+    if (isFinite(monthly)) {
+        monthlyPayment.value = monthly.toFixed(2);
+        totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+        totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+    } else {
+        showError('Please check your numbers');
+    }
+
     e.preventDefault();
+}
+
+//Show error function
+function showError(error) {
+    //create error block
+    const errorDiv = document.createElement('div');
+    errorDiv.innerText = error;
+    errorDiv.className = 'alert alert-danger';
+
+    //Get elements from calculator
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
+
+    //Insert error into DOM
+    card.insertBefore(errorDiv, heading);
+
+    //Clear error after some time
+
+    setTimeout(clearError, 3000);
+
+    function clearError() {
+        document.querySelector('.alert').remove();
+    }
 }
